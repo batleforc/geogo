e70030f78d9d383e9248276cf20b663c58287015
Maxime Leriche <maxleriche.60@gmail.com>
2022-12-09 11:06:37 +0100

    Remove unneased import

--- server/src/main/java/org/example/Geoadd.java
+++ server/src/main/java/org/example/Geoadd.java
@@ -2,14 +2,10 @@ package org.example;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class Geoadd extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res)  throws ServletException, IOException{
